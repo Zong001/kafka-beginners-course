9a5385f564bb53ff96949ba0f664dadfe25caf65
Zong001 <zongchengwang7@gmail.com>
2025-12-19 02:56:13 -0500

    [main] INFO ProducerDemo - H W

--- kafka-basics/src/main/java/io/conductor/demo/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/conductor/demo/kafka/ProducerDemo.java
@@ -1,7 +1,20 @@
 package io.conductor.demo.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
     public static void main(String[] args) {
-        System.out.println("H W");
+        log.info("H W");
+
+        //create Prod properties
+
+        //create Prod
+
+        //send data
+
+        //flush and close Prod
     }
 }
