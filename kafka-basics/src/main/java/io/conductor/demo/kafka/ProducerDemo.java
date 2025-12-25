9efe3c0dc75874436f46d7eab1c191c41aa7109b
Zong001 <zongchengwang7@gmail.com>
2025-12-25 14:44:43 -0500

    set prod prop

--- kafka-basics/src/main/java/io/conductor/demo/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/conductor/demo/kafka/ProducerDemo.java
@@ -1,7 +1,10 @@
 package io.conductor.demo.kafka;
 
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
 
 public class ProducerDemo {
     private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
@@ -10,6 +13,22 @@ public class ProducerDemo {
         log.info("H W");
 
         //create Prod properties
+
+        Properties properties = new Properties();
+
+        //local host
+        properties.setProperty("bootstrap.servers","127.0.0.1:9092");
+
+        //playground
+//        properties.setProperty("security.protocal","SASL_SSL");
+//        properties.setProperty("bootstrap.servers","127.0.0.1:9092");
+//        properties.setProperty("bootstrap.servers","127.0.0.1:9092");
+
+        //set prod prop
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+
 
         //create Prod
 
