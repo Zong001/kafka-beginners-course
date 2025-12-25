d4d61dd11910c4c6e1fe009222c448e701102b4f
Zong001 <zongchengwang7@gmail.com>
2025-12-25 17:00:21 -0500

    StickyPartitioner

--- kafka-basics/src/main/java/io/conductor/demo/kafka/ProducerDemoWithCallback.java
+++ kafka-basics/src/main/java/io/conductor/demo/kafka/ProducerDemoWithCallback.java
@@ -1,15 +1,17 @@
 package io.conductor.demo.kafka;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class ProducerDemo {
-    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+public class ProducerDemoWithCallback {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
 
     public static void main(String[] args) {
         log.info("I am a  ka prod");
@@ -36,13 +38,36 @@ public class ProducerDemo {
 
         KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
 
+
+        for (int i = 0; i < 10;i++){
+
+
         //create Prod Record
 
-        ProducerRecord<String,String> producerRecord = new ProducerRecord<>("demo_java","hello world");
+        ProducerRecord<String,String> producerRecord = new ProducerRecord<>("demo_java","hello world " + i);
 
         //send data
 
-        producer.send(producerRecord);
+        producer.send(producerRecord, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                //exe every time a record sent successfully or an exception thrown
+                if (e == null) {
+                    //success
+                    log.info("Received new metadata \n" +
+                            "Topic " + recordMetadata.topic() + "\n" +
+                            "Partition " + recordMetadata.partition() + "\n" +
+                            "Offset " + recordMetadata.offset() + "\n" +
+                            "TimeStamp " + recordMetadata.timestamp() + "\n"
+                    );
+                }else {
+                    log.error("error wile producing" ,e);
+                }
+            }
+        });
+
+        }
+
 
         // tell the prod to send all data and block until done -- synchronous
 
