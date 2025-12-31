2ddf692e64825bb5a16a5b95f4b2b32a820a8cb6
Zong001 <zongchengwang7@gmail.com>
2025-12-31 04:26:34 -0500

    62. Wikimedia Producer Run

--- kafka-producer-wikiMD/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikiMD/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -11,7 +11,7 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class WikimediaChangesProducer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
 
         String bootstrapServers = "127.0.0.1:9092";
 
